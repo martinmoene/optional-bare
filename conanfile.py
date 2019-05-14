8a5a737f1dbbc298fbd8275368cf0d2a05e42afe
Martin Moene <martin.moene@gmail.com>
2019-05-14 20:05:55 +0200

    [skip ci] Add settings options to conanfile (thanks to @ngrodzitski)
    Settings will be removed from package_id after calling `self.info.header_only()`, but they a helpful for Conan CMake helper to detect generator correctly.
    
    https://github.com/martinmoene/nonstd-lite-project/issues/34

--- conanfile.py
+++ conanfile.py
@@ -7,6 +7,7 @@ class OptionalBareConan(ConanFile):
     license = "Boost Software License - Version 1.0. http://www.boost.org/LICENSE_1_0.txt"
     url = "https://github.com/martinmoene/optional-bare.git"
     exports_sources = "include/nonstd/*", "CMakeLists.txt", "cmake/*", "LICENSE.txt"
+    settings = "compiler", "build_type", "arch"
     build_policy = "missing"
     author = "Martin Moene"
 
